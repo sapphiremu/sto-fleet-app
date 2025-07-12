c61eb333855a4467afdadea1ef046b64c9cad90c
sapphiremu <92813839+sapphiremu@users.noreply.github.com>
2025-07-12 16:16:35 +0200

    Update index.js

--- assets/data/themes/index.js
+++ assets/data/themes/index.js
@@ -1,1 +1,27 @@
+// assets/data/themes/index.js
+export const themeVersion = "0.2";
+
+export default {
+  default: {
+    displayName: "Terran Command",
+    primaryColor: "#B9900C",
+    backgroundColor: "#000000",
+    textColor: "#f00000",
+    borderColor: "#550000",
+    splashImage: require("./default/splash.png"),
+    headerImage: require("./default/header.png"),
+    //icon:      require("./default/icon.png"),
+    titleTextColor: "#00FFFF",
+    titleBackgroundColor: "#700000"
+  },
+  gf: {
+    displayName: "Generic Federation",
+    primaryColor: "#6200EE",
+    backgroundColor: "#121212",
+    textColor: "#0000f0",
+    borderColor: "#000055",
+    titleTextColor: "#A2A3A3",
+    titleBackgroundColor: "#121212"
+  },
+};
 
